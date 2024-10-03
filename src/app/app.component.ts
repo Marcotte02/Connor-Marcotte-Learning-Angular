c6f8b1bd08ef84fa199d960c320ddcef30143eab
Connor Marcotte <CM259@myscc.ca>
2024-10-02 20:46:36 -0400

    Add Sample guitar to top of page * Use GuitarService to get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Guitar } from "./models/guitar";
-import {NgForOf, NgIf} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {GuitarsListComponent} from "./guitars-list/guitars-list.component";
+import {GuitarService} from "./guitar.service";
+import {GuitarsListItemComponent} from "./guitars-list-item/guitars-list-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgForOf, GuitarsListComponent],
+  imports: [RouterOutlet, NgIf, NgForOf, GuitarsListComponent, GuitarsListItemComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string = 'Guitar Store';
+  sampleGuitar?: Guitar;
+
+  constructor(private guitarService: GuitarService) {}
+
+  ngOnInit(): void {
+    this.getSampleGuitar();
+  }
+
+  getSampleGuitar(): void {
+    const id = 1;
+    this.guitarService.getGuitarById(id).subscribe((guitar) => {
+      this.sampleGuitar = guitar;
+    });
+  }
 }
