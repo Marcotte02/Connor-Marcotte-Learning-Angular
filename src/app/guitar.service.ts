860ffa5d345330822efd4217886ff7f470f1edaa
Connor Marcotte <CM259@myscc.ca>
2024-10-02 20:07:47 -0400

    Implement CRUD methods in GuitarService

--- src/app/guitar.service.ts
+++ src/app/guitar.service.ts
@@ -7,10 +7,42 @@ import {GUITARS} from "./data/mock-content";
   providedIn: 'root'
 })
 export class GuitarService {
+  private guitars = GUITARS;
 
   constructor() { }
 
   getGuitars(): Observable<Guitar[]> {
     return of(GUITARS);
+  }
+
+  // Get guitar from ID
+  getGuitarById(guitarId: number): Observable<Guitar | undefined>{
+    const guitar = this.guitars.find(guitar => guitar.id === guitarId);
+    return of(guitar);
+  }
+
+  // Add new guitar to array
+  addGuitar(newGuitar:Guitar): Observable<Guitar[]> {
+    this.guitars.push(newGuitar);
+    return of(this.guitars);
+  }
+
+  // Update an existing guitar
+  updateGuitar(updatedGuitar: Guitar): Observable<Guitar[]> {
+    const index = this.guitars.findIndex(guitar => guitar.id === updatedGuitar.id)
+    if (index !== -1) {
+      this.guitars[index] = updatedGuitar;
+    }
+    return of(this.guitars);
+  }
+
+  // Remove guitar using ID
+  removeGuitar(id: number): Observable<Guitar | undefined> {
+    const index = this.guitars.findIndex(guitar => guitar.id === id);
+    if (index !== -1) {
+      const removedGuitar = this.guitars.splice(index, 1)[0];
+      return of(removedGuitar);
+    }
+    return of(undefined);
   }
 }
