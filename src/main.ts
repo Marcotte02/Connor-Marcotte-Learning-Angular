f94d66d2860c78e6c39fc0ce443af1c2226d0948
Connor Marcotte <CM259@myscc.ca>
2024-10-09 15:25:30 -0400

    Create Router and Routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes} from "@angular/router";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { GuitarsListComponent} from "./app/guitars-list/guitars-list.component";
+import { GuitarsListItemComponent} from "./app/guitars-list-item/guitars-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: 'guitars', component: GuitarsListComponent },
+  { path: 'guitars/:id', component: GuitarsListItemComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'))
